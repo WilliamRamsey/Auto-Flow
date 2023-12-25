ce8304d63ecaab2d28c967ba680def13f4c286be
WilliamRamsey <williamdawsonramsey@gmail.com>
2023-12-24 23:45:17 -0500

    Add argument button for 2AR. Need to copy for other speeches.

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 function addArgument(id) {
     let speech = document.getElementById(id);
 
-    let htmlString = '<div class="aff-argument"><p contenteditable="true">title</p><hr><p contenteditable="true">author</p></div>'
-    let tempDiv = document.createElement("div");
-    tempDiv.innerHTML = htmlString;
+    let containerDiv = document.createElement('div');
+    containerDiv.classList.add('aff-argument')
 
-    let newElements = tempDiv.firstChild.childNodes;
+    let upperParagraph = document.createElement('p');
+    let textNodeUpper = document.createTextNode('title');
+    upperParagraph.appendChild(textNodeUpper);
     
-    let targetElement = document.getElementById(id);
-    newElements.forEach(function(element) {
-        console.log(element)
-        targetElement.appendChild(element);
-    });
+    let lineBreak = document.createElement('hr');
+
+    let lowerParagraph = document.createElement('p');
+    let textNodeLower = document.createTextNode('author');
+    lowerParagraph.appendChild(textNodeLower);
+
+    containerDiv.appendChild(upperParagraph);
+    containerDiv.appendChild(lineBreak);
+    containerDiv.appendChild(lowerParagraph);
+
+    speech.appendChild(containerDiv);
 }
